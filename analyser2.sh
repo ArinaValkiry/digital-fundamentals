b6a6926473ccc03364c8c99861f340e1ef512d67
Arina Aleksandrova <arina3624195@gmail.com>
2025-10-30 15:28:17 +0300

    Add files via upload

---
+++ analyser2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for file in $(ls $1)
+do
+    echo "Info of $1$file"
+    for info in $(cat $1$file)
+    do
+        name=$(echo $info | cut -d ":" -f1)
+        age=$(echo $info | cut -d ":" -f2)
+        email=$(echo $info | cut -d ":" -f3)
+
+        if [[ $email =~ .*@spbstu.ru ]];
+        then
+        echo "Имена: $name, Возраст: $age, Почта: $email"
+        fi
+    done
+
+
+done
---
+++ analyser3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+var17=0
+var18=0
+
+for file in $(ls$1)
+do
+    for info in $(cat$1$file)
+    do
+        age=$(echo $info | cut -d ":" -f2)
+        
+        if [[ $age -ge 18 ]]
+        then 
+            var18=$((var18+1))
+        else
+            var17=$((var17+1))
+        fi
+    done
+done
+
+echo "Количество несовершеннолетних людей - $var17"
+echo "Количество совершеннолетних людей - $var18"
